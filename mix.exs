97dd703d0d4d28a27f32c94c86debf9427325ae7
Alexander Richard Römpke <133158425+AlexanderRoempke@users.noreply.github.com>
2024-05-29 01:03:06 +0200

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -26,7 +26,7 @@ defmodule KinoBumblebee.MixProject do
 
   defp deps do
     [
-      {:bumblebee, "~> 0.5.0"},
+      {:bumblebee, "~> 0.5.3"},
       # {:kino, "~> 0.13"},
       {:kino, github: "livebook-dev/kino", override: true},
       {:nx, "~> 0.7"},
